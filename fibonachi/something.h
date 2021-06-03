068b85aff4af4857dc541ce3d85476c5d89cfc56
Puckemon <e20001573k@yandex.ru>
2021-06-03 15:34:26 +0300

    finding out proper ways for function declaration

--- fibonachi/something.h
+++ fibonachi/something.h
@@ -2,7 +2,7 @@
 #define MY_PROJECT
 
 namespace mp {
-	long long fibonachi(long long n); //type a number - get a fibonacci number with that index
-	void testpointers(int &k, int i); //just testing how pointers work with functions
+	long long fibonachi(long long); //type a number - get a fibonacci number with that index
+	void testpointers(int& , int); //just testing how pointers work with functions
 }
 #endif
